6481f715e600b9a1a40df5ba227b1c04425b757e
Slayer2505 <25754770+Anthane@users.noreply.github.com>
2019-09-02 23:43:05 -0600

    animation finished

---
+++ Assets/_Scripts/Item/Altar.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Altar : MonoBehaviour
+{
+    private  Orb _orb;
+    public TMP_Text myText = null;
+    private bool _orbPlaced = false;
+    public GameObject OrbPoint = null;
+    private Animator OrbPointAnim = null;
+    public GameObject rockLeft = null;
+    public GameObject rockRight = null;
+    private Animator rockLeftAnim = null;
+    private Animator rockRightAnim = null;
+
+    private void Awake()
+    {
+        _orb = GameObject.Find("OrbLight").GetComponent<Orb>();
+        OrbPointAnim = OrbPoint.GetComponent<Animator>();
+        rockLeftAnim = rockLeft.GetComponent<Animator>();
+        rockRightAnim = rockRight.GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider myTrigger)
+    {
+        if (myTrigger.name == "OrbLight" && Input.GetKeyDown(KeyCode.E))
+        {
+            _orb.OrbToAltar();
+            OrbPointAnim.SetTrigger("MoveOrb");
+            rockLeftAnim.SetTrigger("RockMoveLeft");
+            rockRightAnim.SetTrigger("RockMoveRight");
+        }
+        if (_orbPlaced)
+        {
+            myText.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider myTrigger)
+    {
+        if (myTrigger.name == "OrbLight")
+        {
+            myText.gameObject.SetActive(true);
+            myText.text = "Press E To place";
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider myTrigger)
+    {
+
+        if (!_orbPlaced)
+        {
+            if (myTrigger.name == "OrbLight")
+            {
+                myText.gameObject.SetActive(false);
+
+            }
+        }
+       
+    }
+}
---
+++ Assets/_Scripts/Item/Orb.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Orb : MonoBehaviour
+{
+    [SerializeField]
+    Transform playerHoldPoint = null;
+    bool inPlayerHand = false;
+    [SerializeField]
+    private TMP_Text myText= null;
+    [SerializeField]
+    Transform altarPoint = null;
+    private bool inAltar = false;
+
+    
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inAltar)
+        {
+            if (inPlayerHand == true)
+            {
+                transform.position = playerHoldPoint.position;
+            }
+        }
+        
+    }
+
+    private void OnTriggerStay(Collider myCollider)
+    {
+        if (!inAltar)
+        {
+            if (myCollider.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+            {
+
+                this.gameObject.transform.parent = playerHoldPoint.transform;
+                inPlayerHand = true;
+                if (myText.gameObject.activeInHierarchy == true)
+                {
+                    myText.gameObject.SetActive(false);
+                }
+            }
+        }
+        
+        
+    }
+
+    private void OnTriggerEnter(Collider myTrigger)
+    {
+        if (!inAltar)
+        {
+            if (inPlayerHand == true)
+            {
+                return;
+            }
+            if (myTrigger.CompareTag("Player"))
+            {
+                myText.gameObject.SetActive(true);
+                myText.text = "Press E";
+            }
+        }
+        
+        
+    }
+
+    private void OnTriggerExit(Collider myTrigger)
+    {
+        if (!inAltar)
+        {
+            if (inPlayerHand == true)
+            {
+                return;
+            }
+            if (myTrigger.CompareTag("Player"))
+            {
+                myText.gameObject.SetActive(false);
+            }
+        }
+        
+    }
+
+    public void OrbToAltar()
+    {
+        inPlayerHand = false;
+        inAltar = true;
+        this.gameObject.transform.parent = altarPoint.transform;
+        transform.position = altarPoint.position;
+    }
+}
